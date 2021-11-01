dadebc244a1a01fa4c0c744b7a4cb32461a277f1
LuisPulenta <luisalbertonu@gmail.com>
2021-11-01 13:57:48 -0300

    Show menu

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,12 @@
+import 'dart:convert';
+
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:vehicles_app/helpers/constants.dart';
+import 'package:vehicles_app/models/token.dart';
+import 'package:vehicles_app/components/loader_component.dart';
+import 'package:vehicles_app/screens/home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -8,27 +16,51 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  String _email = '';
-  String _password = '';
+  String _email = 'luis@yopmail.com';
+  String _emailError = '';
+  bool _emailShowError = false;
+
+  String _password = '123456';
+  String _passwordError = '';
+  bool _passwordShowError = false;
+
   bool _rememberme = true;
+
+  bool _passwordShow = false;
+
+  bool _showLoader = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFE4D359),
       appBar: AppBar(
         title: Text('Login'),
       ),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
         children: <Widget>[
-          _showLogo(),
-          _showEmail(),
-          _showPassword(),
-          _showRememberme(),
-          _showButtons(),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _showLogo(),
+                SizedBox(
+                  height: 40,
+                ),
+                _showEmail(),
+                _showPassword(),
+                _showRememberme(),
+                _showButtons(),
+              ],
+            ),
+          ),
+          _showLoader
+              ? LoaderComponent(
+                  text: 'Por favor espere...',
+                )
+              : Container(),
         ],
-      )),
+      ),
     );
   }
 
@@ -45,8 +77,12 @@ class _LoginScreenState extends State<LoginScreen> {
       child: TextField(
         keyboardType: TextInputType.emailAddress,
         decoration: InputDecoration(
+            fillColor: Colors.white,
+            filled: true,
             hintText: 'Ingresa tu Email...',
             labelText: 'Email',
+            errorText: _emailShowError ? _emailError : null,
+            prefixIcon: Icon(Icons.alternate_email),
             suffixIcon: Icon(Icons.email),
             border:
                 OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
@@ -62,16 +98,28 @@ class _LoginScreenState extends State<LoginScreen> {
     return Container(
       padding: EdgeInsets.all(10),
       child: TextField(
-        obscureText: true,
+        obscureText: !_passwordShow,
         decoration: InputDecoration(
+            fillColor: Colors.white,
+            filled: true,
             hintText: 'Ingresa tu Contraseña...',
             labelText: 'Contraseña',
-            suffixIcon: Icon(Icons.lock),
+            errorText: _passwordShowError ? _passwordError : null,
+            prefixIcon: Icon(Icons.lock),
+            suffixIcon: IconButton(
+              icon: _passwordShow
+                  ? Icon(Icons.visibility)
+                  : Icon(Icons.visibility_off),
+              onPressed: () {
+                setState(() {
+                  _passwordShow = !_passwordShow;
+                });
+              },
+            ),
             border:
                 OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
         onChanged: (value) {
           _password = value;
-          print(_password);
         },
       ),
     );
@@ -104,7 +152,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   return Color(0xFF120E43);
                 }),
               ),
-              onPressed: () {},
+              onPressed: () => _login(),
             ),
           ),
           SizedBox(
@@ -125,5 +173,89 @@ class _LoginScreenState extends State<LoginScreen> {
         ],
       ),
     );
+  }
+
+  void _login() async {
+    setState(() {
+      _passwordShow = false;
+    });
+
+    if (!validateFields()) {
+      return;
+    }
+
+    setState(() {
+      _showLoader = true;
+    });
+
+    Map<String, dynamic> request = {
+      'userName': _email,
+      'password': _password,
+    };
+
+    var url = Uri.parse('${Constants.apiUrl}/api/Account/CreateToken');
+    var response = await http.post(
+      url,
+      headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json',
+      },
+      body: jsonEncode(request),
+    );
+
+    setState(() {
+      _showLoader = false;
+    });
+
+    if (response.statusCode >= 400) {
+      setState(() {
+        _passwordShowError = true;
+        _passwordError = 'Email o contraseña incorrectos';
+      });
+      return;
+    }
+
+    var body = response.body;
+    var decodedJson = jsonDecode(body);
+    var token = Token.fromJson(decodedJson);
+
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (context) => HomeScreen(
+                  token: token,
+                )));
+  }
+
+  bool validateFields() {
+    bool isValid = true;
+
+    if (_email.isEmpty) {
+      isValid = false;
+      _emailShowError = true;
+      _emailError = 'Debes ingresar tu Email';
+    } else if (!EmailValidator.validate(_email)) {
+      isValid = false;
+      _emailShowError = true;
+      _emailError = 'Debes ingresar un Email válido';
+    } else {
+      _emailShowError = false;
+    }
+
+    if (_password.isEmpty) {
+      isValid = false;
+      _passwordShowError = true;
+      _passwordError = 'Debes ingresar tu Contraseña';
+    } else if (_password.length < 6) {
+      isValid = false;
+      _passwordShowError = true;
+      _passwordError = 'La Contraseña debe tener al menos 6 caracteres';
+    } else {
+      _passwordShowError = false;
+    }
+
+    setState(() {});
+
+    return isValid;
   }
 }
